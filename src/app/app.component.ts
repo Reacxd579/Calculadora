35c6b06b9c108602f47812167c9174916fab5819
Ricardo Aguilar <reacxd579@gmail.com>
2023-09-11 10:46:00 -0600

    assa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tarea';
+  activeItem: string = 'calculadora'; // Elemento activo inicial
+  iconColor: string = '#ffffff'; // Color de iconos inicial
+  queztalesIconColor: string = '#ffffff'; // Color de iconos de "Queztales" inicial
+  queztalesSelected: string = 'Seleccione moneda'; // Texto inicial en "Queztales"
+
+  // Método para cambiar el elemento activo y el color de los iconos
+  setActiveItem(item: string) {
+    this.activeItem = item;
+    this.iconColor = '#000000'; // Cambiar el color de los iconos a negro
+  }
+
+  // Método para cambiar el color de los iconos de "Queztales" y su texto
+  setQueztales(iconColor: string, selected: string) {
+    this.queztalesIconColor = iconColor;
+    this.queztalesSelected = selected;
+  }
 }
